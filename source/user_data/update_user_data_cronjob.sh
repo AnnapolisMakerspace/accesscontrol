fbf1a7dcad015a9ae90953dc44fbdd6c6683cfa5
Dan Hahne <giphahne@gmail.com>
2017-06-28 10:53:41 -0400

    clearing key

--- source/user_data/update_user_data_cronjob.sh
+++ source/user_data/update_user_data_cronjob.sh
@@ -2,7 +2,7 @@
 export user_data_dir="/home/pi/source/user_data"
 cd $user_data_dir
 
-${user_data_dir}/get_contacts.sh '778pma2x79s0lsk68b3vphk3q9dxin' | \
+${user_data_dir}/get_contacts.sh '<---redacted--->' | \
 	${user_data_dir}/parse_contact_data.sh > \
 					${user_data_dir}/ams_user_data.tmp
 
